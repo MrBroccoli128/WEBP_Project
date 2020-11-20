8ca1ffd532c5c509e2ee3698f5bd4595b447c576
MrBroccoli128 <jonathan.linder@stud.htwchur.ch>
2020-11-20 08:54:46 +0100

    Entire rework of project

--- static/scripts.js
+++ static/scripts.js
@@ -5,62 +5,72 @@ $(function () {
   const volumenSEE = 200000;
   const volumenROCHETOWER = 324000;
 
-  /* 2.8 Millionen Tonnen an Food Waste in der Schweiz pro Jahr in KG */
-  let totalWasteKG = 2800000000;
+  let seitenlaenge
 
-  /* Umrechnung pro Liter = 1kg Abfall */
-  let volumentrash = totalWasteKG / 1000; /* m3 */
 
   /* Canvas definieren */
-  c = document.getElementById("grafic");
-  ctx = c.getContext("2d");
+  tc = document.getElementById("grafic");
+  tctx = tc.getContext("2d");
 
   /* Canvas für Referenz */
   r = document.getElementById("referencebuilding");
   rtx = r.getContext("2d");
 
-
-  // Initales Trash rect zeichnen, mit den Buttons wird 
-  trashrect();
-
-
   /*FUNCTIONS */
-  function trashrect() {
-    ctx.clearRect(0, 0, c.width, c.height);
-    let seitenlaenge = Math.floor(Math.cbrt(volumentrash));
-
-    ctx.beginPath();
-    ctx.lineWidth = "1";
-    ctx.strokeStyle = "black";
-    ctx.rect(5, 5, seitenlaenge, seitenlaenge);
-    ctx.stroke();
 
 
+  /* Draw Rect
+  Parameters:
+  Element
+  Context
+  length
+  linecolor
+  
+  */
+function drawrect(canv, cont, length,linecol){
+
+  canv.clearRect(0, 0, cont.width, cont.height);
+  canv.beginPath();
+  canv.lineWidth = "1";
+  canv.strokeStyle = linecol; //color
+  canv.rect(5, 5, length, length);
+  canv.stroke();
+}  
+
+
+  function getcbrt(volume){
+    return Math.floor(Math.cbrt(volume));
   }
 
 
-  $('.btn').click(function () {
+  $('.reference').click(function () {
     let id = $(this).attr('id');
-    let seitenlaenge
+
     switch (id) {
       case "primetower":
-        seitenlaenge = Math.floor(Math.cbrt(volumenPRIMETOWER));
+        drawrect(rtx, r, getcbrt(volumenPRIMETOWER), "blue");
+        $('#referencetitle').text("Der Prime Tower");
         break;
-      case "rochetower":
-        seitenlaenge = Math.floor(Math.cbrt(volumenROCHETOWER));
+      case "rochetower":   
+        drawrect(rtx, r, getcbrt(volumenROCHETOWER), "blue");
         break;
-        case "see":
-          seitenlaenge = Math.floor(Math.cbrt(volumenSEE));
-          break;
+      case "see":
+        drawrect(rtx, r, getcbrt(volumenSEE), "blue");
+        break;
     }
-    rtx.clearRect(0, 0, r.width, r.height);
-    rtx.beginPath();
-    rtx.lineWidth = "1";
-    rtx.strokeStyle = "blue";
-    rtx.rect(5, 5, seitenlaenge, seitenlaenge);
-    rtx.stroke();
+  });
+
+  $('#recalc').click(function() {
+
+    tctx.clearRect(0, 0, tc.width, tc.height);
+    seitenlaenge = getcbrt(($("#foodwasteschweiz").val())/1000);
+    drawrect(tctx, tc, seitenlaenge, "black");
 
   });
+  
+  //ON LOAD
+  // Draw inital trash rect 
+  $('#recalc').click();
 
 
 });
