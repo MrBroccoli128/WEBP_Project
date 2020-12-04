e8af8e1e7f1294ba3bdbfd4412340ac89d66df4f
MrBroccoli128 <jonathan.linder@stud.htwchur.ch>
2020-12-04 16:24:27 +0100

    Feedback rework

--- static/data_objects.js
+++ static/data_objects.js
@@ -1,34 +1,42 @@
-
-  //Quelle der Daten zu Food Waste
-  var schweiz = {
-      foodwastequantity: 2800000,
-      countryname: "Schweiz",
-      references: {
-          reference1: {
-              name: "Prime Tower",
-              volumen: 228000,
-              image_link: "",
-          },
-          reference2: {
-              name: "Roche Tower",
-              volumen: 324000,
-              image_link: ""
-          },
-          reference3: {
-              name: "Cresta See",
-              volumen: 300000,
-              image_link: ""
-          }
-      }
-  };
-  var deutschland = {
-    foodwastequantity: 12708000, 
+//Quelle der Daten zu Food Waste: Statista
+var schweiz = {
+    foodwastequantity: 2800000,
+    foodwastequantity_in_T: function() {
+        return ((this.foodwastequantity)/1000000);
+    },
+    countryname: "der Schweiz",
+    countrytext: function() {
+        return "Die Schweiz produziert pro Jahr rund " + (this.foodwastequantity_in_T) + " Millionen Tonnen Foodwaste.";
+    },
+    references: {
+        reference1: {
+            name: "Prime Tower",
+            volumen: 228000,
+            image_link: "./images/Prime_Tower.png",
+        },
+        reference2: {
+            name: "Roche Tower",
+            volumen: 324000,
+            image_link: "./images/roche_tower.jpg"
+        },
+        reference3: {
+            name: "Cresta See",
+            volumen: 300000,
+            image_link: "./images/cresta_see.jpg"
+        }
+    }
+};
+var deutschland = {
+    foodwastequantity: 12708000,
     countryname: "Deutschland",
+    countrytext: function () {
+            return "me no care";
+    },
     references: {
         reference1: {
             name: "Kölner Dom",
             volumen: 407000, // https://www.stadtgeschichten-stadtfuehrungen.koeln/index.php/tipps-fuer-ihren-koeln-trip/koeln-wissen/342-koelner-dom-gigant-aus-stein#:~:text=Die%20Gesamtl%C3%A4nge%20betr%C3%A4gt%20au%C3%9Fen%20145,200.000%20m%C2%B3%20(407.000%20m%C2%B3).
-            image_link: ""
+            image_link: "./images/koelner_dom.jpg"
         },
         reference2: {
             name: "Chiem See",
@@ -43,13 +51,16 @@
     }
 };
 var italien = {
-    foodwastequantity: 8772000, 
+    foodwastequantity: 8772000,
     countryname: "Italien",
+    countrytext: function () {
+        return "me o ned care";
+    },
     references: {
         reference1: {
             name: "Peters Dom",
             volumen: 1200000,
-            image_link: ""
+            image_link: "./images/st_peters_dom.jpg"
         },
         reference2: {
             name: "Italien Ref 2",
--- static/reference_graphics.js
+++ static/reference_graphics.js
@@ -2,15 +2,14 @@ $(function () {
 
 
   /* Canvas definieren */
-  tc = document.getElementById("grafic");
-  tctx = tc.getContext("2d");
+  c = document.getElementById("grafic");
+  ctx = c.getContext("2d");
 
-  /* Canvas für Referenz */
-  r = document.getElementById("referencebuilding");
+  r = document.getElementById("grafic_ref");
   rtx = r.getContext("2d");
 
-  // Set default Foodwaste value
-  $("#foodwaste").val(volumeFOODWASTE);
+
+
 
   /*FUNCTIONS */
   /* Draw Rect
@@ -23,6 +22,8 @@ $(function () {
   */
   function drawrect(canv, cont, length, linecol) {
 
+
+
     canv.clearRect(0, 0, cont.width, cont.height);
     canv.beginPath();
     canv.lineWidth = "1";
@@ -33,61 +34,118 @@ $(function () {
 
   function getcbrt(volume) {
     return Math.floor(Math.cbrt(volume));
-  }n 
+  }
+
   $('.reference').click(function () {
     let id = $(this).attr('id');
     let countyselect = $('input[name="contryselection"]:checked').attr('id');
-    alert(id + " " + countyselect);
     $('#text_referencetitle').text(eval(countyselect)["references"][id]["name"]);
+    
+
+    //draw cube
+    drawCube(rtx, r, 150, 480, getcbrt(eval(countyselect)["references"][id]["volumen"]), getcbrt(eval(countyselect)["references"][id]["volumen"]), getcbrt(eval(countyselect)["references"][id]["volumen"]), getcbrt(eval(countyselect)["references"][id]["volumen"]), getcbrt(eval(countyselect)["references"][id]["volumen"]));
+
     //image einblenden
+    $('#img_reference').attr("src", eval(countyselect)["references"][id]["image_link"]).height(300).width(300);
     //titel anpassen
+    $('#title_reference_cube').text("Volumen des " + eval(countyselect)["references"][this.id]["name"]);
     //Text anpassen
+    $('#text_foodwastebeschreibung').text(eval(countyselect).countrytext);
+
   });
 
-  $('input[type=radio][name="contryselection"]').change(function() {
+  $('input[type=radio][name="contryselection"]').change(function () {
 
-   let countyselect = $('input[name="contryselection"]:checked').attr('id');
+    let countyselect = $('input[name="contryselection"]:checked').attr('id');
     //Update Buttons
     $('div[class="referenceselector"]').children('button').each(function () {
-      $("#"+this.id).text(eval(countyselect)["references"][this.id]["name"]);
+      $("#" + this.id).text(eval(countyselect)["references"][this.id]["name"]);
+    });
+
+    //DRAW CUBE
+    drawCube(ctx, c, 250, 480, getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity));
+    
+    //Country Name Update 
+    $('.text_country').text(eval(countyselect).countryname);
+
+   
+    
+
+    //klick auf reference1 damit es initialisiert wird
+    $('.reference').click();
   });
+
+
+
+
+  //Migration auf 3d Model
+  function drawCube(canvas, context, x, y, wx, wy, h) {
+
+    canvas.clearRect(0, 0, context.width, context.height);
+
+    // LINE MODE
+    canvas.lineJoin = "round";
+    
+    // left face
+    canvas.beginPath();
+    canvas.moveTo(x, y);
+    canvas.lineTo(x - wx, y - wx * 0.5);
+    canvas.lineTo(x - wx, y - h - wx * 0.5);
+    canvas.lineTo(x, y - h * 1);
+    canvas.closePath();
+    canvas.fillStyle = "#838357"
+    canvas.strokeStyle = "#7a7a51";
+    canvas.stroke();
+    canvas.fill();
+
+    // right face
+    canvas.beginPath();
+    canvas.moveTo(x, y);
+    canvas.lineTo(x + wy, y - wy * 0.5);
+    canvas.lineTo(x + wy, y - h - wy * 0.5);
+    canvas.lineTo(x, y - h * 1);
+    canvas.closePath();
+    canvas.fillStyle = "#6f6f49";
+    canvas.strokeStyle = "#676744";
+    canvas.stroke();
+    canvas.fill();
+
+    // center face
+    canvas.beginPath();
+    canvas.moveTo(x, y - h);
+    canvas.lineTo(x - wx, y - h - wx * 0.5);
+    canvas.lineTo(x - wx + wy, y - h - (wx * 0.5 + wy * 0.5));
+    canvas.lineTo(x + wy, y - h - wy * 0.5);
+    canvas.closePath();
+    canvas.fillStyle = "#989865";
+    canvas.strokeStyle = "#8e8e5e";
+    canvas.stroke();
+    canvas.fill();
+}
+
+
+
+$('input[type=radio][name="contryselection"]').change();
+
+// ---------------------------------------------------------------------
+
+function inRange (num, start, end) {
+	return num >= start && num <= end;
+}
+
+$('input[type=range]').change(function () {
   
-  //Draw Rectangle 
-  drawrect(tctx, tc, getcbrt(eval(countyselect).foodwastequantity), "black");
+  let fw_in_kg = this.value;
+  let kg_to_chf = 620/330;
+  let fw_in_chf = Math.floor(this.value * kg_to_chf);
+  $('#slider_val').text((this.value) + " KG");
 
-  //Country Text Update
+  $('#fw_val_in_chf').text((fw_in_chf) + " CHF");
 
-  });
+  inRange(this.value, 400, 600)
+  $('#slider_item').attr('src', './images/img_slider/ps5.jpg').height(250).width(400);
 
-
-//Migration auf 3d Model
-  function drawCube(x, y, wx, wy, h) {
-    tctx.beginPath();
-    tctx.moveTo(x, y);
-    tctx.lineTo(x - wx, y - wx * 0.5);
-    tctx.lineTo(x - wx, y - h - wx * 0.5);
-    tctx.lineTo(x, y - h * 1);
-    tctx.closePath();
-    tctx.stroke();
-    tctx.fill();
-
-    tctx.beginPath();
-    tctx.moveTo(x, y);
-    tctx.lineTo(x + wy, y - wy * 0.5);
-    tctx.lineTo(x + wy, y - h - wy * 0.5);
-    tctx.lineTo(x, y - h * 1);
-    tctx.closePath();
-    tctx.stroke();
-    tctx.fill();
-
-    tctx.beginPath();
-    tctx.moveTo(x, y - h);
-    tctx.lineTo(x - wx, y - h - wx * 0.5);
-    tctx.lineTo(x - wx + wy, y - h - (wx * 0.5 + wy * 0.5));
-    tctx.lineTo(x + wy, y - h - wy * 0.5);
-    tctx.closePath();
-    tctx.stroke();
-    tctx.fill();
-  }
+});
+$('input[type=range]').change();
 
 });
