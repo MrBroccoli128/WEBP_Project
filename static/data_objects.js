c9abfb12312259b3f7e337eae22a068efb8c9025
MrBroccoli128 <jonathan.linder@stud.htwchur.ch>
2020-12-08 18:45:16 +0100

    Content Update

--- static/data_objects.js
+++ static/data_objects.js
@@ -1,76 +1,120 @@
 //Quelle der Daten zu Food Waste: Statista
 var schweiz = {
     foodwastequantity: 2800000,
-    foodwastequantity_in_T: function() {
-        return ((this.foodwastequantity)/1000000);
-    },
     countryname: "der Schweiz",
-    countrytext: function() {
-        return "Die Schweiz produziert pro Jahr rund " + (this.foodwastequantity_in_T) + " Millionen Tonnen Foodwaste.";
-    },
     references: {
         reference1: {
             name: "Prime Tower",
             volumen: 228000,
             image_link: "./images/Prime_Tower.png",
+            text: "Prime Tower ist ein Hochhaus in Zürich. Das 126 Meter hohe Bauwerk mit 36 Stockwerken hat ein Volumen von 228.000m³."
         },
         reference2: {
             name: "Roche Tower",
             volumen: 324000,
-            image_link: "./images/roche_tower.jpg"
+            image_link: "./images/roche_tower.jpg",
+            text: "Der Roche Tower in Basel ist mit 178m das höchste Gebäude der Schweiz und verfügt über ein Volumen von 324.000m³."
         },
         reference3: {
             name: "Cresta See",
             volumen: 300000,
-            image_link: "./images/cresta_see.jpg"
+            image_link: "./images/cresta_see.jpg",
+            text: "Der Crestasee liegt zwischen Flims und Trin im schweizerischen Kanton Graubünden auf 844 m Höhe. Der See ist 280m lang und hat eine maximal Tiefe von 14m. Das das Wasservolumen beträgt 300.000m³."
         }
     }
 };
+
 var deutschland = {
     foodwastequantity: 12708000,
     countryname: "Deutschland",
-    countrytext: function () {
-            return "me no care";
-    },
     references: {
         reference1: {
             name: "Kölner Dom",
             volumen: 407000, // https://www.stadtgeschichten-stadtfuehrungen.koeln/index.php/tipps-fuer-ihren-koeln-trip/koeln-wissen/342-koelner-dom-gigant-aus-stein#:~:text=Die%20Gesamtl%C3%A4nge%20betr%C3%A4gt%20au%C3%9Fen%20145,200.000%20m%C2%B3%20(407.000%20m%C2%B3).
-            image_link: "./images/koelner_dom.jpg"
+            image_link: "./images/koelner_dom.jpg",
+            text: "Die Gesamtlänge beträgt außen 145 m, (Kölner Dom 144,58 m). Die Höhe des Mittelschiffes beträgt 42,30 m (43,35 m). Die Fläche auf der die Kathedrale ruht, hat die Ausmaße von 7.700 m² (7.914 m²), das Volumen ohne Strebewerk beträgt 200.000 m³ (407.000 m³)."
         },
         reference2: {
-            name: "Chiem See",
-            volumen: 2047000,
-            image_link: ""
+            name: "Alianz Arena",
+            volumen: 2928300,
+            image_link: "./images/alianzarena.jpg",
+            text: "Das Volumen der Allianz-Arena schlägt Daumen mal Pi mit 2.928.300m³ (258m x 227m x 50m) zu Buche"
         },
         reference3: {
             name: "DE Ref 3",
             volumen: 300000,
-            image_link: ""
+            image_link: "",
+            text: ""
         }
     }
 };
+
 var italien = {
     foodwastequantity: 8772000,
     countryname: "Italien",
-    countrytext: function () {
-        return "me o ned care";
-    },
     references: {
         reference1: {
             name: "Peters Dom",
             volumen: 1200000,
-            image_link: "./images/st_peters_dom.jpg"
+            image_link: "./images/st_peters_dom.jpg",
+            text: "Die ‘Basilica di San Pietro in Vaticano’ oder der Petersdom wurde zwischen 1506 und 1626 erbaut. Der Dom beeindruckt vor allem durch seine Größe (136 Meter hoch und 186 x 123 Meter breit)."
         },
         reference2: {
-            name: "Italien Ref 2",
-            volumen: 324000,
-            image_link: ""
+            name: "Kolosseum",
+            volumen: 1105632,
+            image_link: "./images/kolosseum.jpg",
+            text: "Das Kolosseum ist ellipsenförmig gebaut. Seine Breite beträgt 156 Meter, die Länge 188 Meter, der Umfang 527 Meter, die Höhe 48 Meter. Auch der Boden der Arena war elliptisch mit einer Breite von 54 Metern und einer Länge von 86 Metern."
         },
         reference3: {
             name: "Italien Ref 3",
             volumen: 300000,
-            image_link: ""
+            image_link: "",
+            text: ""
         }
+    }
+};
+
+var sparpotential = {
+    item1: { // <100 CHF
+        min: 0,
+        max: 100,
+        title: "Taschengeld",
+        desc: "Taschengeld für die Kinder für ein ganzes Jahr.",
+        image_link: "./images/img_slider/taschengeld.jpg"
+    },
+    item2: { // 101 - 300 CHF
+        min: 101,
+        max: 300,
+        title: "Grosseinkauf",
+        desc: "Dieser Betrag reicht für einen Grosseinkauf in der Schweiz.",
+        image_link: "./images/img_slider/einkaufswagen.jpg",
+    },
+    item3: { // 301 - 500 CHF
+        min: 301,
+        max: 500,
+        title: "Playstation 5",
+        desc: "Eine Playstation 5 wäre doch viel cooler",
+        image_link: "./images/img_slider/ps5.jpg"
+    },
+    item4: { // 501 - 700 CHF
+        min: 501,
+        max: 700,
+        title: "Grafikkarte",
+        desc: "Für dieses Geld hätte ich mir lieber eine neue Grafikkarte für meinen PC gekauft.",
+        image_link: "./images/img_slider/gk.jpg"
+    },
+    item5: { // 701 - 900 CHF
+        min: 701,
+        max: 900,
+        title: "Netflix oder Spotify",
+        desc: "Also ich hätte lieber für 5 Jahre mein Netflix oder Spotify bezahlt als das ganze Essen fortgeworfen....",
+        image_link: "./images/img_slider/Netflix.png"
+    },
+    item6: { // > 901 CHF
+        min: 901,
+        max: 2000,
+        title: "Ferien",
+        desc: "Wären wir doch lieber in die Karibik gefolgen als unser gutes Essen wegzuwerfen.",
+        image_link: "./images/img_slider/karibik.jpeg"
     }
 };
--- static/reference_graphics.js
+++ static/reference_graphics.js
@@ -8,32 +8,17 @@ $(function () {
   r = document.getElementById("grafic_ref");
   rtx = r.getContext("2d");
 
-
-
-
-  /*FUNCTIONS */
-  /* Draw Rect
-  Parameters:
-  Element
-  Context
-  length
-  linecolor
-  
-  */
-  function drawrect(canv, cont, length, linecol) {
-
-
-
-    canv.clearRect(0, 0, cont.width, cont.height);
-    canv.beginPath();
-    canv.lineWidth = "1";
-    canv.strokeStyle = linecol; //color
-    canv.rect(5, 5, length, length);
-    canv.stroke();
-  }
-
   function getcbrt(volume) {
     return Math.floor(Math.cbrt(volume));
+  }
+
+  function calcrelation(country,reference) {
+    return (country/reference).toFixed(1);
+  }
+
+  // SOURCE: https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+  function numberWithCommas(x) { 
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
   $('.reference').click(function () {
@@ -48,26 +33,30 @@ $(function () {
     //image einblenden
     $('#img_reference').attr("src", eval(countyselect)["references"][id]["image_link"]).height(300).width(300);
     //titel anpassen
-    $('#title_reference_cube').text("Volumen des " + eval(countyselect)["references"][this.id]["name"]);
+    $('.title_reference_cube').text(eval(countyselect)["references"][this.id]["name"]);
     //Text anpassen
-    $('#text_foodwastebeschreibung').text(eval(countyselect).countrytext);
+    $('#text_reference_cube').text(eval(countyselect)["references"][this.id]["text"]);
+    // Set relation sentence
+    $('#text_reference_rel').text(" Der Food Waste von " + eval(countyselect).countryname + " passt somit " + calcrelation(eval(countyselect).foodwastequantity,eval(countyselect)["references"][this.id]["volumen"]) + " in den/die " + eval(countyselect)["references"][this.id]["name"] + ".");
 
   });
 
   $('input[type=radio][name="contryselection"]').change(function () {
 
     let countyselect = $('input[name="contryselection"]:checked').attr('id');
+    let clength = getcbrt(eval(countyselect).foodwastequantity);
     //Update Buttons
-    $('div[class="referenceselector"]').children('button').each(function () {
+    $('div[class="referenceselector form-inline justify-content-center"]').children('button').each(function () {
       $("#" + this.id).text(eval(countyselect)["references"][this.id]["name"]);
     });
 
     //DRAW CUBE
-    drawCube(ctx, c, 250, 480, getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity), getcbrt(eval(countyselect).foodwastequantity));
+    drawCube(ctx, c, 250, 480, clength, clength, clength, clength, clength);
     
     //Country Name Update 
     $('.text_country').text(eval(countyselect).countryname);
-
+    $('.text_fw_in_T').text(numberWithCommas(eval(countyselect).foodwastequantity));
+    $('.text_q_length').text(clength);
    
     
 
@@ -78,7 +67,7 @@ $(function () {
 
 
 
-  //Migration auf 3d Model
+  //Source: https://stackoverflow.com/questions/44829028/html5-canvas-correctly-draw-a-cube
   function drawCube(canvas, context, x, y, wx, wy, h) {
 
     canvas.clearRect(0, 0, context.width, context.height);
@@ -125,7 +114,6 @@ $(function () {
 
 
 
-$('input[type=radio][name="contryselection"]').change();
 
 // ---------------------------------------------------------------------
 
@@ -133,19 +121,43 @@ function inRange (num, start, end) {
 	return num >= start && num <= end;
 }
 
+function getmin (id) {
+  return sparpotential["item" + id]["min"];
+}
+
+function getmax (id) {
+  return sparpotential["item" + id]["max"];
+}
+
 $('input[type=range]').change(function () {
   
-  let fw_in_kg = this.value;
   let kg_to_chf = 620/330;
   let fw_in_chf = Math.floor(this.value * kg_to_chf);
-  $('#slider_val').text((this.value) + " KG");
+  let i = 1;
 
+  // Number Updater
+  $('#slider_val').text((this.value) + " KG");
   $('#fw_val_in_chf').text((fw_in_chf) + " CHF");
 
-  inRange(this.value, 400, 600)
-  $('#slider_item').attr('src', './images/img_slider/ps5.jpg').height(250).width(400);
+  while (true) {
+    if (inRange(fw_in_chf, getmin(i), getmax(i)) == true) {
+      //alert(i); 
+      // Update Title and Texts
+      $('#slider_item_title').text(sparpotential["item" + i]["title"]);
+      $('#slider_item_text').text(sparpotential["item" + i]["desc"]);
+      // Insert Image
+      $('#slider_item_img').attr('src', sparpotential["item" + i]["image_link"]).height(250).width(400);
+
+      //Stop loop
+      break;
+    } else { // not in this range
+      i++;
+    }
+  }
 
 });
-$('input[type=range]').change();
 
+// Initialisierung
+$('input[type=range]').change();
+$('input[type=radio][name="contryselection"]').change();
 });
