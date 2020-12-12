91b139957f97a21b6ee7f5d757a8b0663b633f42
MrBroccoli128 <jonathan.linder@stud.htwchur.ch>
2020-12-12 11:24:04 +0100

    Beautify

--- static/reference_graphics.js
+++ static/reference_graphics.js
@@ -12,23 +12,23 @@ $(function () {
   rtx = r.getContext("2d");
 
 
-/* FUNCTION getcbrt
-  Berechnung der Würfelseitelänge
-  volumen:  Volumen in Kubikmeter m3
-  Berechung der Seitenlänge durch das Ziehen der dritten Wurzel.
-*/
+  /* FUNCTION getcbrt
+    Berechnung der Würfelseitelänge
+    volumen:  Volumen in Kubikmeter m3
+    Berechung der Seitenlänge durch das Ziehen der dritten Wurzel.
+  */
   function getcbrt(volume) {
     return Math.floor(Math.cbrt(volume));
   }
 
-/* FUNTION calcrelation
-  Berechung des Grössenverhältnis zwischen Landes- und Referenzvolumen
-  Gerundet auf eine Kommastelle
-  country:   Food Wastevolumen des ausgewählten Landes
-  reference: Food Wastevolumen des Referenzgebäudes
-*/
-  function calcrelation(country,reference) {
-    return (country/reference).toFixed(1);
+  /* FUNTION calcrelation
+    Berechung des Grössenverhältnis zwischen Landes- und Referenzvolumen
+    Gerundet auf eine Kommastelle
+    country:   Food Wastevolumen des ausgewählten Landes
+    reference: Food Wastevolumen des Referenzgebäudes
+  */
+  function calcrelation(country, reference) {
+    return (country / reference).toFixed(1);
   }
 
   /* FUNCTION numberWithCommas
@@ -37,15 +37,15 @@ $(function () {
   x:  Zahl als String, welche die Kommasetzung erfordert
   SOURCE: https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
   */
-  function numberWithCommas(x) { 
+  function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
 
-/*
-  CLICK TRIGGER
-  Anpassung der Seite wenn ein anderes Referenzgebäude ausgewählt.
-*/
+  /*
+    CLICK TRIGGER
+    Anpassung der Seite wenn ein anderes Referenzgebäude ausgewählt.
+  */
   $('.reference').click(function () {
     // Die ID enthält den ausgewählten Button, um das Referenzgebäude einzublenden
     let id = $(this).attr('id');
@@ -63,12 +63,12 @@ $(function () {
     // Text anpassen
     $('#text_reference_cube').text(eval(countyselect)["references"][this.id]["text"]);
     // Set relation sentence
-    $('#text_reference_rel').text("Der Food Waste von " + eval(countyselect).countryname + " passt somit " + calcrelation(eval(countyselect).foodwastequantity,eval(countyselect)["references"][this.id]["volumen"]) + " in den/die " + eval(countyselect)["references"][this.id]["name"] + ".");
+    $('#text_reference_rel').text("Der Food Waste von " + eval(countyselect).countryname + " passt somit " + calcrelation(eval(countyselect).foodwastequantity, eval(countyselect)["references"][this.id]["volumen"]) + " in den/die " + eval(countyselect)["references"][this.id]["name"] + ".");
 
   });
 
 
- /* CHANGE TRIGGER
+  /* CHANGE TRIGGER
   Anpassung der Seite wenn ein anderes Land ausgesucht wird.
 */
   $('input[type=radio][name="contryselection"]').change(function () {
@@ -83,7 +83,7 @@ $(function () {
 
     // Landeswürfelzeichnen
     drawCube(ctx, c, 250, 480, getcbrt(eval(countyselect).foodwastequantity));
-    
+
     // Country Name Update in diversen Texten
     $('.text_country').text(eval(countyselect).countryname);
 
@@ -92,7 +92,7 @@ $(function () {
 
     // Angabe der Würfelseitenlänge
     $('.text_q_length').text(getcbrt(eval(countyselect).foodwastequantity));
-   
+
     // Klick auf den ersten Button damit das erste Referenzgebäude initialisiert wird
     $('#reference1').click();
   });
@@ -120,7 +120,7 @@ $(function () {
 
     // LINE MODE
     canvas.lineJoin = "round";
-    
+
     // Linke Seite
     canvas.beginPath();
     canvas.moveTo(x, y);
@@ -156,82 +156,82 @@ $(function () {
     canvas.strokeStyle = "#8e8e5e";
     canvas.stroke();
     canvas.fill();
-}
-
-
-
-
-// ------------------------------ ZWEITE FUNKTION ---------------------------------------
-
-
-
-/* FUNCTION InRange
-Überprüfen ob Wert innerhalb eines Bereichs ist
-num:  Wert, welcher überprüft wird
-start: Start Range
-end:   End Range
-
-return -> bool
-*/
-function inRange (num, start, end) {
-	return num >= start && num <= end;
-}
-
-/* FUNCTION getmin
-Zugriff auf Minimumwert des Items im data_objects.js aus dem Object sparpotential
-id: ITEM ID
-*/
-function getmin (id) {
-  return sparpotential["item" + id]["min"];
-}
-
-/* FUNCTION getmax
-Zugriff auf Maxwert des Items im data_objects.js aus dem Object sparpotential
-id: ITEM ID
-*/
-function getmax (id) {
-  return sparpotential["item" + id]["max"];
-}
-
-
- /* CHANGE TRIGGER
-  Bewegung des Sliders 
-*/
-$('input[type=range]').change(function () {
-  
-  // Initalies CHF to KG Verhältnis 
-  let kg_to_chf = 620/330;
-
-  // Berechnung des Werts der ausgewählten Menge Food Waste
-  let fw_in_chf = Math.floor(this.value * kg_to_chf);
-
-  //Counter
-  let i = 1;
-
-  // Number Update
-  $('#slider_val').text((this.value) + " KG");
-  $('#fw_val_in_chf').text((fw_in_chf) + " CHF");
-
-  //Loopen bis der richtige Range für den ausgewählten Wert gefunden wurde
-  while (true) {
-
-    if (inRange(fw_in_chf, getmin(i), getmax(i)) == true) {
-      // Update Title and Texts mit Daten aus dem sparpotential Objekt
-      $('#slider_item_title').text(sparpotential["item" + i]["title"]);
-      $('#slider_item_text').text(sparpotential["item" + i]["desc"]);
-      // Insert Image
-      $('#slider_item_img').attr('src', sparpotential["item" + i]["image_link"]).height(250).width(400);
-
-      //Stop loop
-      break;
-    } else { // not in this range
-      i++;
-    }
   }
 
-});
 
-// Initialisierung
-$('input[type=range]').change();
-$('input[type=radio][name="contryselection"]').change();
+
+
+  // ------------------------------ ZWEITE FUNKTION ---------------------------------------
+
+
+
+  /* FUNCTION InRange
+  Überprüfen ob Wert innerhalb eines Bereichs ist
+  num:  Wert, welcher überprüft wird
+  start: Start Range
+  end:   End Range
+
+  return -> bool
+  */
+  function inRange(num, start, end) {
+    return num >= start && num <= end;
+  }
+
+  /* FUNCTION getmin
+  Zugriff auf Minimumwert des Items im data_objects.js aus dem Object sparpotential
+  id: ITEM ID
+  */
+  function getmin(id) {
+    return sparpotential["item" + id]["min"];
+  }
+
+  /* FUNCTION getmax
+  Zugriff auf Maxwert des Items im data_objects.js aus dem Object sparpotential
+  id: ITEM ID
+  */
+  function getmax(id) {
+    return sparpotential["item" + id]["max"];
+  }
+
+
+  /* CHANGE TRIGGER
+  Bewegung des Sliders 
+*/
+  $('input[type=range]').change(function () {
+
+    // Initalies CHF to KG Verhältnis 
+    let kg_to_chf = 620 / 330;
+
+    // Berechnung des Werts der ausgewählten Menge Food Waste
+    let fw_in_chf = Math.floor(this.value * kg_to_chf);
+
+    //Counter
+    let i = 1;
+
+    // Number Update
+    $('#slider_val').text((this.value) + " KG");
+    $('#fw_val_in_chf').text((fw_in_chf) + " CHF");
+
+    //Loopen bis der richtige Range für den ausgewählten Wert gefunden wurde
+    while (true) {
+
+      if (inRange(fw_in_chf, getmin(i), getmax(i)) == true) {
+        // Update Title and Texts mit Daten aus dem sparpotential Objekt
+        $('#slider_item_title').text(sparpotential["item" + i]["title"]);
+        $('#slider_item_text').text(sparpotential["item" + i]["desc"]);
+        // Insert Image
+        $('#slider_item_img').attr('src', sparpotential["item" + i]["image_link"]).height(250).width(400);
+
+        //Stop loop
+        break;
+      } else { // not in this range
+        i++;
+      }
+    }
+
+  });
+
+  // Initialisierung
+  $('input[type=range]').change();
+  $('input[type=radio][name="contryselection"]').change();
 });
